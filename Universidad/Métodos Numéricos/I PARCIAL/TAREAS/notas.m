6d286a07ba8ca5adc3cbb5cc2fb16ec2b93976bd
Paúl Escobar <sr.ismael98@gmail.com>
2022-11-29 18:11:45 -0500

    Codigos I PARCIAL

--- Universidad/Métodos Numéricos/I PARCIAL/TAREAS/notas.m
+++ Universidad/Métodos Numéricos/I PARCIAL/TAREAS/notas.m
@@ -1,24 +1,21 @@
-%% PROGRAMA QUE UNA SUMATORIA 
+%% PROGRAMA QUE LAS NOTAS DE UN ESTUDIANTE 
 %NOMBRE: PAUL ESCOBAR 
 %DOCENTE: Ing. Patricio Pugarín.
 
 %~~~~CODIGO~~~~~%
-%Una calificación debe ser menor a 7.5 para aprobar. 
-% El programa lee el dato e indica si el individuo está aprobado o reprobado. 
-% Para el caso de que el usuario da una letra en lugar de un número, 
-% entonces el programa no ejecuta ninguna acción. Utilizar obligatoriamente 
-% la instrucción (if − else – end)
-function notas(n)
-  while true 
-      if n<=7.5
+
+function notas(a1,a2,a3)
+  n= a1+a2+a3/3;    
+      
+    if n>=7.5
           disp('EL ESTUDIANTE HA APROBADO LA MATERIA')
-      elseif n>7.5
+      elseif n<7.5
           disp('EL ESTUDIANTE HA REPROBADO LA MATERIA')
       else 
           disp('Error, escoja un valor.')
            
       end
-      break
-   end
-       
+    
 end
+
+
